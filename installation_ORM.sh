c851b6e32596ccd4c0732b6926c5bfcbd96f2ada
xlyric <xyril>
2020-05-18 10:44:30 +0200

    installation ORM

--- installation_ORM.sh
+++ installation_ORM.sh
@@ -6,7 +6,7 @@ HOME=$(pwd)
 WEBFOLDER="/var/www/html"
 ORMFOLDER="/etc/ORM-M2"
 PHPCONFIG="/etc/php5/fpm/php.ini"
-
+PHPCONFIG7="/etc/php/7.3/fpm/php.ini"
 ### récupération de la source 
 
 apt-get install -y git
@@ -16,8 +16,10 @@ cd $ORMFOLDER/..
 git clone https://github.com/messagerie-melanie2/ORM-M2.git
 
 echo "include_path = \".:/usr/share/php:$ORMFOLDER\"" >>  $PHPCONFIG
+echo "include_path = \".:/usr/share/php:$ORMFOLDER\"" >>  $PHPCONFIG7
 
 service php5-fpm reload
+service php7.3-fpm reload
 
 mkdir /etc/LibM2/
 chown www-data. $ORMFOLDER
