30bb00cb1d4fc9902a3fec9e91db8cae664ea3c3
xlyric <xyril>
2020-05-20 09:57:56 +0200

    correction apache7install

--- installation_apache_php7.sh
+++ installation_apache_php7.sh
@@ -4,15 +4,16 @@
 apt-get update
 apt-get install -y apache2  php php-fpm php-memcached php-mbstring memcached
 
-#conf  php avec Nginx
+#conf  php avec apache
 cp test.php /var/www/html/
 a2dismod mpm_prefork
 a2enmod mpm_event
 a2enmod proxy_fcgi
-service apache2 restart
 
 ### 
-cp  000-default.conf /etc/apache2/sites-enabled/000-default.conf
+cp  000-default.conf /etc/apache2/sites-available/
+service apache2 restart
+
 
 # conf memcache
 cp memcached.conf /etc/memcached.conf
